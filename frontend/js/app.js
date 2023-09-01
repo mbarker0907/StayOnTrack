b03452ad1761d41418b201ebde05ce152d375236
Michael Barker <mbarker0907@gmail.com>
2023-09-01 11:43:01 +0200

    Added functionality to mark tasks as complete with strikethrough effect and refined CSS for better visualization.

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,16 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    // Getting references to the elements
     const addTaskBtn = document.getElementById('add-task-btn');
     const newTaskInput = document.getElementById('new-task-input');
     const taskList = document.getElementById('task-list');
 
+    // Adding a click event listener to the add task button
     addTaskBtn.addEventListener('click', () => {
+        
+        // Getting and trimming the task input value
         const taskValue = newTaskInput.value.trim();
         
+        // Checking if the task input value is not empty
         if(taskValue) {
+            
+            // Creating a new list item element for the task
             const taskItem = document.createElement('li');
-            taskItem.innerText = taskValue;
+            
+            // Creating a checkbox input element
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            
+            // Adding an event listener to the checkbox to handle task completion
+            checkbox.addEventListener('change', function() {
+                if (this.checked) {
+                    taskItem.classList.add('completed-task');
+                } else {
+                    taskItem.classList.remove('completed-task');
+                }
+            });
+
+            // Appending the checkbox to the task item
+            taskItem.appendChild(checkbox);
+            
+            // Creating a span to hold the task text
+            const taskText = document.createElement('span');
+            taskText.innerText = ` ${taskValue}`;
+
+            // Appending the task text to the task item
+            taskItem.appendChild(taskText);
+            
+            // Appending the task item to the task list
             taskList.appendChild(taskItem);
-            newTaskInput.value = ''; // Clear the input
+            
+            // Clearing the input field for the next task
+            newTaskInput.value = ''; 
         }
     });
 });
