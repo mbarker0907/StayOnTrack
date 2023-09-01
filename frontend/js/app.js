f1d23952cef093111a39db549932087102adbeaa
Michael Barker <mbarker0907@gmail.com>
2023-09-01 19:57:43 +0200

    Enhance Task Management UI and Logic
    CSS Changes:
    - Introduced a smoother adeIn animation for the appearance of new tasks.
    - Adjusted the styles for consistency and improved visuals, focusing on buttons and task items.
    - Improved layout of task item containers for better visual separation and user experience.
    
    JavaScript Changes:
    - Reorganized the script to break down functionalities into modular helper functions.
    - Implemented the createTaskItem function to manage the creation and appending of new task items.
    - Established the createConfirmationPrompt function to handle the delete confirmation dialog dynamically.
    - Optimized the task completion toggle using the classList.toggle method.
    - Included inline comments for clarity, ensuring future maintainability and understanding of the codebase.

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -3,7 +3,37 @@ document.addEventListener('DOMContentLoaded', () => {
     const newTaskInput = document.getElementById('new-task-input');
     const taskList = document.getElementById('task-list');
 
-    const createConfirmationPrompt = (taskItem) => {
+    const createTaskItem = (taskValue) => {
+        const taskItemContainer = document.createElement('div');
+        taskItemContainer.classList.add('task-item-container');
+
+        // Checkbox for completed task
+        const taskCompleteCheckbox = document.createElement('input');
+        taskCompleteCheckbox.type = 'checkbox';
+        taskCompleteCheckbox.addEventListener('change', () => {
+            taskItem.classList.toggle('completed-task', taskCompleteCheckbox.checked);
+        });
+        taskItemContainer.appendChild(taskCompleteCheckbox);
+
+        // The task itself
+        const taskItem = document.createElement('li');
+        taskItem.innerText = taskValue;
+        taskItemContainer.appendChild(taskItem);
+
+        // Delete button
+        const deleteBtn = document.createElement('button');
+        deleteBtn.innerText = "x";
+        deleteBtn.classList.add('delete-btn');
+        deleteBtn.addEventListener('click', () => {
+            createConfirmationPrompt(taskItemContainer);
+        });
+        taskItemContainer.appendChild(deleteBtn);
+
+        // Append to task list
+        taskList.appendChild(taskItemContainer);
+    };
+
+    const createConfirmationPrompt = (taskItemContainer) => {
         const promptDiv = document.createElement('div');
         promptDiv.classList.add('confirmation-prompt');
 
@@ -11,16 +41,21 @@ document.addEventListener('DOMContentLoaded', () => {
         message.innerText = "Are you sure you want to delete this task?";
         promptDiv.appendChild(message);
 
+        // Yes button
         const yesButton = document.createElement('button');
         yesButton.innerText = "✔";
         yesButton.classList.add('confirmation-btn');
         yesButton.style.color = 'green';
         yesButton.addEventListener('click', () => {
-            taskList.removeChild(taskItem);
+            taskItemContainer.style.animation = "slideFadeOut 0.5s forwards";
+            setTimeout(() => {
+                taskList.removeChild(taskItemContainer);
+            }, 500); 
             document.body.removeChild(promptDiv);
         });
         promptDiv.appendChild(yesButton);
 
+        // No button
         const noButton = document.createElement('button');
         noButton.innerText = "✖";
         noButton.classList.add('confirmation-btn');
@@ -30,31 +65,14 @@ document.addEventListener('DOMContentLoaded', () => {
         });
         promptDiv.appendChild(noButton);
 
-        // Append the promptDiv to the body directly
+        // Append the prompt to the body
         document.body.appendChild(promptDiv);
     };
 
     addTaskBtn.addEventListener('click', () => {
         const taskValue = newTaskInput.value.trim();
-
         if (taskValue) {
-            const taskItemContainer = document.createElement('div');
-            taskItemContainer.classList.add('task-item-container');
-
-            const taskItem = document.createElement('li');
-            taskItem.innerText = taskValue;
-
-            const deleteBtn = document.createElement('button');
-            deleteBtn.innerText = "x";
-            deleteBtn.classList.add('delete-btn');
-            deleteBtn.addEventListener('click', () => {
-                createConfirmationPrompt(taskItemContainer);
-            });
-
-            taskItemContainer.appendChild(taskItem);
-            taskItemContainer.appendChild(deleteBtn);
-            taskList.appendChild(taskItemContainer);
-
+            createTaskItem(taskValue);
             newTaskInput.value = ''; 
         }
     });
